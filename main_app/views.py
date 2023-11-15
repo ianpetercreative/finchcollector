678576010570f365a53959e986e78d2574ca8a44
ianmacdonald <ianpetercreative@gmail.com>
2023-11-15 10:14:07 -0700

    add routes, functions, and html for home, about, and all finches user stories. Add base.html file with boilerplate. Add template inheritance to home, about, and all finches.

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+# will be replaced by a Model and DB information eventually 
+finches = [
+    {'name': 'Sky', 'breed': 'Zebra Finch', 'description': 'cheerful singer', 'age': 1},
+    {'name': 'Sunny', 'breed': 'Gouldian Finch', 'description': 'vibrant plumage', 'age': 2},
+    {'name': 'Pippin', 'breed': 'Java Sparrow', 'description': 'playful and social', 'age': 1},
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
