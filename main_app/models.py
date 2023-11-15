708decd2db9a4c782dd2fb299262c6dc6bd2db54
ianmacdonald <ianpetercreative@gmail.com>
2023-11-15 15:24:58 -0700

    add Finch model with a __str__ function. Register the Finch model in admin.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# finches = [
+#     {'name': 'Sky', 'breed': 'Zebra Finch', 'description': 'cheerful singer', 'age': 1},
+#     {'name': 'Sunny', 'breed': 'Gouldian Finch', 'description': 'vibrant plumage', 'age': 2},
+#     {'name': 'Pippin', 'breed': 'Java Sparrow', 'description': 'playful and social', 'age': 1},
+# ]
+
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name 
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
+from .models import Finch
 
-# will be replaced by a Model and DB information eventually 
-finches = [
-    {'name': 'Sky', 'breed': 'Zebra Finch', 'description': 'cheerful singer', 'age': 1},
-    {'name': 'Sunny', 'breed': 'Gouldian Finch', 'description': 'vibrant plumage', 'age': 2},
-    {'name': 'Pippin', 'breed': 'Java Sparrow', 'description': 'playful and social', 'age': 1},
-]
 
 # Create your views here.
 def home(request):
@@ -15,6 +10,7 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
     })
