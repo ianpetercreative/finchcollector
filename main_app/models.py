360b6214637cad12b1494c14d316faec77d8de5e
ianmacdonald <ianpetercreative@gmail.com>
2023-11-19 14:55:08 -0700

    add toys model. add toys page. add page to add toys. display toys on the finch detail page.

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
+from datetime import date
+
 # Create your models here.
 MEALS = (
     ('B', 'Breakfast'),
@@ -8,17 +10,32 @@ MEALS = (
     ('D', 'Dinner'),
 )
 
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
+    
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return self.name 
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
+    
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
     
 
 class Feeding(models.Model):
