82059d42aec528aa37bc319054a15e157b7ff1a9
ianmacdonald <ianpetercreative@gmail.com>
2023-11-19 13:03:06 -0700

    Add feedings to the detail view along with a form to add more feedings

--- main_app/models.py
+++ main_app/models.py
@@ -29,7 +29,10 @@ class Feeding(models.Model):
         default=MEALS[0][0]
     )
 
-    Finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.get_meal_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
